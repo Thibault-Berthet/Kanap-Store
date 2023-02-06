5d7fc00be53c17a20ec6c04ce045fd250562008e
Thibault <115456866+Thibault-Berthet@users.noreply.github.com>
2023-02-06 19:33:01 +0100

    Local Storage

--- front/js/product.js
+++ front/js/product.js
@@ -2,8 +2,10 @@
 
 const urlSearchParams = new URLSearchParams(window.location.search);
 // URLSearchParams est fourni par JS
+
 // Variable en camelCase
 // Class en PascalCase
+
 // Window c'est toute ma fenetre navigateur
 // Window.location c'est la barre de recherche
 // Window.location.search c'est l'entierete de l'URL
@@ -20,6 +22,37 @@ fetch(`http://localhost:3000/api/products/${urlSearchParams.get("id")}`)
 // Attend la réponse du JSON ? Le response correspond à une réponse ?
     .then((response) => response.json())
 
+    // .then(data => data.map(imageC =>
+    //     `<img src="${imageC.imageUrl}" alt="${imageC.altTxt}">`
+    //     ))
+
     .then(canap => {
-        document.getElementById("title").innerHTML = canap.name
+
+        document.querySelector(".item__img").innerHTML = `<img src="${canap.imageUrl}" alt="${canap.altTxt}">`;
+
+        document.getElementById("title").innerHTML = canap.name;
+        document.getElementById("price").innerHTML = canap.price;
+        document.getElementById("description").innerHTML = canap.description;
+
+        // let options = "";
+
+        // for (let i = 0; i < canap.colors.length; i++){
+        //     // document.getElementById("colors").innerHTML = 
+        //     // += garde la valeur précédente et ajoute la suivante dedans
+        //     options += `<option value="${canap.colors[i]}">-- ${canap.colors[i]} --</option>`;
+        // }
+
+        document.getElementById("colors").innerHTML = canap.colors.map( color => `<option value="${color}">-- ${color} --</option>` ).join('');
+        // Fait la meme chose que la boucle for
+
     })
+
+document.addEventListener("click", () => {
+    console.log('test');
+    // localStorage.clear()
+
+    let objCanap = [`${urlSearchParams.get('id')}`, document.getElementsByTagName('color-select'), document.getElementsByTagName('itemQuantity')]
+
+    let donneeCanap = JSON.stringify(objCanap)
+    localStorage.setItem('obj',donneeCanap)
+})
